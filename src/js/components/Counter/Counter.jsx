448eaf0c8f0908bee3e7bd96b638391606c40f73
AlexMaz99 <ola_m10@onet.pl>
2021-04-01 23:19:52 +0200

    removed linter and prettier

--- src/js/components/Counter/Counter.jsx
+++ src/js/components/Counter/Counter.jsx
@@ -3,16 +3,12 @@ import { useSelector, useDispatch } from 'react-redux';
 import allActions from '../../actions';
 
 const Counter = () => {
-  // useSelector() - extract data from the Redux store state, using a selector function
   const counter = useSelector((state) => state.counterReducer);
-
-  // useDispatch() - returns a reference to the dispatch function from the Redux store
   const dispatch = useDispatch();
 
   return (
     <div>
       <h1>Counter: {counter}</h1>
-      {/* dispatch() - run reducer function by the store and calculate the new state */}
       <button onClick={() => dispatch(allActions.counterActions.decrement())}>
         Decrease Counter
       </button>
