a7a14c45af8d676d26a246519aeaa5b4edf76a37
AlexMaz99 <ola_m10@onet.pl>
2021-04-05 23:59:29 +0200

    SCH-76; added switch button

--- src/ts/views/Example/Example.tsx
+++ src/ts/views/Example/Example.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import { useState } from 'react';
+import SwitchButton from '../../components/common/SwitchButton/SwitchButton';
 import Counter from '../../components/Counter/Counter';
 import UserLogin from '../../components/UserLogin/UserLogin';
 
 import styles from './Example.module.css';
 
 const Example = () => {
+  const [showText, setShowText] = useState(true);
+
   return (
     <div className={styles.example}>
       <UserLogin />
       <Counter />
+      <SwitchButton
+        className={styles.switchButton}
+        onChange={() => setShowText(!showText)}
+        title={'Example switch'}
+      />
+      {showText ? <p>Switch on</p> : <p>Switch off</p>}
     </div>
   );
 };
