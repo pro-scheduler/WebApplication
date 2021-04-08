88ecd1627e1a1790e4ed1f18f572924039670f13
Radoslaw Kopec <radek4ec@gmail.com>
2021-04-09 00:52:13 +0200

    remove comment

--- src/ts/actions/meetingActions.ts
+++ src/ts/actions/meetingActions.ts
@@ -1,20 +1,6 @@
 import Meeting from "../model/Meeting";
 import { Dispatch } from 'redux';
 
-// function makeASandwichWithSecretSauce(forPerson) {
-//     // We can invert control here by returning a function - the "thunk".
-//     // When this function is passed to `dispatch`, the thunk middleware will intercept it,
-//     // and call it with `dispatch` and `getState` as arguments.
-//     // This gives the thunk function the ability to run some logic, and still interact with the store.
-//     return function(dispatch) {
-//       return fetchSecretSauce().then(
-//         (sauce) => dispatch(makeASandwich(forPerson, sauce)),
-//         (error) => dispatch(apologize('The Sandwich Shop', forPerson, error)),
-//       );
-//     };
-//   }
-
-
 const fetchAllMeetings = () => (dispatch:Dispatch) => {
    fetch("http://localhost:8080/api/meetings")
         .then(response => response.json())
@@ -25,7 +11,7 @@ const fetchAllMeetings = () => (dispatch:Dispatch) => {
 
 const loadMeeting = (id: number) => {
   return {
-      type: "LOAD_ALL",
+      type: "LOAD_ONE",
       payload: id
   }
 }
@@ -39,7 +25,7 @@ const addMeeting = (meeting: Meeting) => {
   
   const deleteMeeting = (id: number) => {
     return {
-      type: 'LOG_OUT',
+      type: 'DELETE_MEETING',
       payload: id
     }
   }
