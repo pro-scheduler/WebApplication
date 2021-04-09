4c2e7c714aa6f32397aeb3d4ed4f32f262ccf2df
Radoslaw Kopec <radek4ec@gmail.com>
2021-04-09 18:07:41 +0200

    enable prettier, format some files

--- src/ts/actions/meetingActions.ts
+++ src/ts/actions/meetingActions.ts
@@ -1,49 +1,52 @@
-import Meeting from "../model/Meeting";
+import Meeting from '../model/Meeting';
 import { Dispatch } from 'redux';
 
-const fetchAllMeetings = () => (dispatch:Dispatch) => {
-   fetch("http://localhost:8080/api/meetings")
-        .then(response => response.json())
-        .then( meetings =>  {console.log(meetings); return dispatch({type: "LOAD_ALL", payload: meetings})}
-        //   (error) => dispatch(apologize('The Sandwich Shop', forPerson, error)),
-        );
-}
+const fetchAllMeetings = () => (dispatch: Dispatch) => {
+  fetch('http://localhost:8080/api/meetings')
+    .then((response) => response.json())
+    .then(
+      (meetings) => {
+        console.log(meetings);
+        return dispatch({ type: 'LOAD_ALL', payload: meetings });
+      }
+      //   (error) => dispatch(apologize('The Sandwich Shop', forPerson, error)),
+    );
+};
 
 const loadMeeting = (id: number) => {
   return {
-      type: "LOAD_ONE",
-      payload: id
-  }
-}
+    type: 'LOAD_ONE',
+    payload: id,
+  };
+};
 
 const addMeeting = (meeting: Meeting) => {
-    return {
-      type: 'ADD_MEETING',
-      payload: meeting,
-    }
-  }
-  
-  const deleteMeeting = (id: number) => {
-    return {
-      type: 'DELETE_MEETING',
-      payload: id
-    }
-  }
+  return {
+    type: 'ADD_MEETING',
+    payload: meeting,
+  };
+};
 
-  const updateMeeting = (meeting: Meeting) =>{
-    return {
-        type: 'UPDATE_MEETING',
-        payload: meeting
-      }
-  }
-  
-  const actions = { 
-    addMeeting,
-    deleteMeeting,
-    updateMeeting,
-    fetchAllMeetings,
-    loadMeeting
- };
-  
-  export default actions;
-  
+const deleteMeeting = (id: number) => {
+  return {
+    type: 'DELETE_MEETING',
+    payload: id,
+  };
+};
+
+const updateMeeting = (meeting: Meeting) => {
+  return {
+    type: 'UPDATE_MEETING',
+    payload: meeting,
+  };
+};
+
+const actions = {
+  addMeeting,
+  deleteMeeting,
+  updateMeeting,
+  fetchAllMeetings,
+  loadMeeting,
+};
+
+export default actions;
--- src/ts/views/CreateMeeting/CreateMeeting.tsx
+++ src/ts/views/CreateMeeting/CreateMeeting.tsx
@@ -1,27 +1,33 @@
-import {useEffect} from 'react';
+import { useEffect } from 'react';
 import actions from '../../actions/meetingActions';
 import { useDispatch, useSelector } from 'react-redux';
 import Meeting from '../../model/Meeting';
 
 interface RootState {
-  meetingReducer: Meeting[]
+  meetingReducer: Meeting[];
 }
 
 const CreateMeeting = () => {
-
   const dispatch = useDispatch();
-  const meetings = useSelector((state: RootState) => {console.log("refresh:"); console.log(state); return state.meetingReducer} )
+  const meetings = useSelector((state: RootState) => {
+    console.log('refresh:');
+    console.log(state);
+    return state.meetingReducer;
+  });
 
-  useEffect( () => {
+  useEffect(() => {
     dispatch(actions.fetchAllMeetings());
+    // eslint-disable-next-line
   }, []);
   // empty [] allows to treat useEffect() as componentDidMount in class components, https://stackoverflow.com/questions/56249151/react-useeffect-hook-componentdidmount-to-useeffect
-    return <p>
+  return (
+    <p>
       {/* tmp need implement view in SCH-65 */}
       {Object.keys(meetings).map((value, index) => {
-        return index + ", "
+        return index + ', ';
       })}
-    </p>;
-}
+    </p>
+  );
+};
 
 export default CreateMeeting;
