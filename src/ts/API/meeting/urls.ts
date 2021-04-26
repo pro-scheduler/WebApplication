c27acfe7f73527a699360bb634330ad05c86b566
Radoslaw Kopec <radek4ec@gmail.com>
2021-04-26 16:52:20 +0200

    add production ports

--- src/ts/API/meeting/urls.ts
+++ src/ts/API/meeting/urls.ts
@@ -1,7 +1,7 @@
-export const getMeetingsUrl = () => 'http://localhost:8080/api/meetings';
+export const getMeetingsUrl = () => 'http://localhost:9000/api/meetings';
 
 export const getMeetingUrl = (meetingId: number) =>
-  `http://localhost:8080/api/meetings/${meetingId}`;
+  `http://localhost:9000/api/meetings/${meetingId}`;
 
 export const getMeetingTimeRangesUrl = (meetingId: number) =>
   getMeetingUrl(meetingId) + '/timeranges';
