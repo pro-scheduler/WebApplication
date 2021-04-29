8eaae4507b849c62eb17a79fc96356a97361adec
AlexMaz99 <ola_m10@onet.pl>
2021-04-29 17:48:52 +0200

    SCH-133; added popup

--- src/ts/components/MeetingDetails/UserIcon.tsx
+++ src/ts/components/MeetingDetails/UserIcon.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FcManager } from 'react-icons/fc';
 import { TiDelete } from 'react-icons/ti';
 
 import './UserIcon.css';
 import allActions from '../../actions';
+import Popup from '../common/Popup/Popup';
 
 interface IUserIcon {
   name: String;
@@ -14,8 +15,10 @@ interface IUserIcon {
 }
 
 const UserIcon = (user: IUserIcon) => {
-  const onClick = () => {
+  const [modalShow, setModalShow] = useState(false);
+  const deleteParticipant = () => {
     allActions.meetingActions.removeUserFromMeeting(user.meetingId, user.userId);
+    setModalShow(false);
   };
   return (
     <>
@@ -27,7 +30,15 @@ const UserIcon = (user: IUserIcon) => {
       ) : (
         <div className="ml-3 userIconParticipantName">{user.name}</div>
       )}
-      {user.canDelete && <TiDelete className="ml-2 deleteUserButton" onClick={onClick} />}
+      {user.canDelete && (
+        <TiDelete className="ml-2 deleteUserButton" onClick={() => setModalShow(true)} />
+      )}
+      <Popup
+        show={modalShow}
+        information={'Are you sure you want to remove the participant from the meeting?'}
+        onDecline={() => setModalShow(false)}
+        onAccept={deleteParticipant}
+      />
     </>
   );
 };
