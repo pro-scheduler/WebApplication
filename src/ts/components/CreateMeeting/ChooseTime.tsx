6b8c8da188127598b5affdb74627a421d157b5f9
Radoslaw Kopec <radek4ec@gmail.com>
2021-05-19 20:34:57 +0200

    Add responsiviti to grid

--- src/ts/components/CreateMeeting/ChooseTime.tsx
+++ src/ts/components/CreateMeeting/ChooseTime.tsx
@@ -6,9 +6,11 @@ import DayPicker, { DateUtils, DayModifiers } from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 import { useState } from 'react';
 import TimePicker from '../TimeGrid/TimePicker';
+import useWindowDimensions from '../common/window/WindowDimension';
 
 const ChooseTime = () => {
   const [selectedDays, setSelectedDays] = useState<Date[]>([]);
+  const { height, width } = useWindowDimensions();
 
   const handleDayClick = (day: Date, { selected }: DayModifiers) => {
     const days = selectedDays.concat();
@@ -23,23 +25,29 @@ const ChooseTime = () => {
   };
 
   return (
-    <div>
+    <div style={{ marginLeft: width < 576 ? 0 : 45 }}>
       <Row className="justify-content-center mt-5">
         <Col xs="auto">
           <CalendarIcon />
         </Col>
       </Row>
-      <Row className="justify-content-center my-4">
+      <Row className="justify-content-center mt-4">
         <div className={style.createHeader}>Set time of the meeting</div>
       </Row>
-      <Row>
+      <Row className="justify-content-center mt-4">
         <Col />
         <Col className="text-center">
           <DayPicker selectedDays={selectedDays} onDayClick={handleDayClick} />
+          {width}
         </Col>
         <Col />
       </Row>
-      <TimePicker days={selectedDays} />
+      <div style={{ marginRight: width < 576 ? 45 : 0 }}>
+        <TimePicker
+          days={selectedDays}
+          count={width > 1290 ? 4 : width > 991 ? 3 : width > 660 ? 2 : 1}
+        />
+      </div>
     </div>
   );
 };
--- src/ts/components/TimeGrid/TimePicker.tsx
+++ src/ts/components/TimeGrid/TimePicker.tsx
@@ -9,13 +9,13 @@ import styles from './TimePicker.module.css';
 
 const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-const TimePicker = ({ days }: { days: Date[] }) => {
+const TimePicker = ({ days, count }: { days: Date[]; count: number }) => {
   const [currentDays, setCurrentDays] = useState<JSX.Element[]>([]);
   const [start, setStart] = useState<number>(0);
 
   useEffect(() => {
     const tmp = days.map((day: Date, index: number) => (
-      <Col hidden={!(index >= start && index < start + 4)} key={index}>
+      <Col hidden={!(index >= start && index < start + count)} key={index}>
         <TimeGrid
           primaryLabel={('0' + day.getDate()).slice(-2) + '.' + ('0' + day.getMonth()).slice(-2)}
           secondaryLabel={weekDays[day.getDay()]}
@@ -23,31 +23,31 @@ const TimePicker = ({ days }: { days: Date[] }) => {
         />
       </Col>
     ));
-    while (tmp.length < 4) {
+    while (tmp.length < count) {
       tmp.push(<Col key={tmp.length} />);
     }
     setCurrentDays(tmp);
-  }, [start, days]);
+  }, [start, days, count]);
 
   return (
-    <Container fluid className="ml-5 ml-sm-auto">
-      <Row className="justify-content-center mt-4">
-        <div className={styles.leftArrow}>
-          {days.length > 4 && (
+    // <Container className="ml-5 ml-sm-auto">
+    <Row className="justify-content-center" style={{ position: 'relative', marginLeft: 10 }}>
+      {/* <div className={styles.leftArrow}>
+          {days.length > count && (
             <NextLeftButton onclick={() => setStart(start - 1)} disabled={start === 0} />
           )}
-        </div>
-        {currentDays}
-        <div className={styles.rightArrow}>
-          {days.length > 4 && (
+        </div> */}
+      {currentDays}
+      {/* <div className={styles.rightArrow}>
+          {days.length > count && (
             <NextRightButton
               onclick={() => setStart(start + 1)}
-              disabled={start >= days.length - 4}
+              disabled={start >= days.length - count}
             />
           )}
-        </div>
-      </Row>
-    </Container>
+        </div> */}
+    </Row>
+    // </Container>
   );
 };
 
