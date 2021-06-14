dfc361a1830f64ed484df3b732bfd3867fa8f209
Marcin Zielonka <dev.zielonka.marcin@gmail.com>
2021-06-14 22:43:49 +0200

    SCH-161; fix

--- src/ts/auth/AuthCredentials.ts
+++ src/ts/auth/AuthCredentials.ts
@@ -1,3 +1,3 @@
-const googleUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=621232719841-7v4dq1335o23j626us79bph9p2ji6nho.apps.googleusercontent.com&redirect_uri=${process.env.HOST}/api/auth/callback&scope=https://www.googleapis.com/auth/userinfo.email&state=fj74nx1631`;
+const googleUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=621232719841-7v4dq1335o23j626us79bph9p2ji6nho.apps.googleusercontent.com&redirect_uri=${process.env.REACT_APP_AUTH_HOST}/api/auth/callback&scope=https://www.googleapis.com/auth/userinfo.email&state=fj74nx1631`;
 
 export default googleUrl;
