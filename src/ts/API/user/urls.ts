8abc59af09d56119c2eb2f1a675aa69d0dfa5597
AlexMaz99 <ola_m10@onet.pl>
2021-06-29 11:26:50 +0200

    changed url in user meetings

--- src/ts/API/user/urls.ts
+++ src/ts/API/user/urls.ts
@@ -3,9 +3,9 @@ export const getUsersUrl = () => `${process.env.REACT_APP_API_URL}users`;
 export const getUserUrl = (userId: number) => getUsersUrl() + `/${userId}`;
 
 export const getUserParticipatedMeetingsUrl = (userId: number) =>
-  getUserUrl(userId) + '/meetings?organized=false';
+  getUserUrl(userId) + '/meetings?roles=attendee';
 
 export const getUserOrganizedMeetingsUrl = (userId: number) =>
-  getUserUrl(userId) + '/meetings?organized=true';
+  getUserUrl(userId) + '/meetings?roles=organizer';
 
 export const getCurrentUserUrl = () => `${getUsersUrl()}/me`;
