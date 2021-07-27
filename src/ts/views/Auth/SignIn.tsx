f3032f3674aa4386e6d9e018e76ad601e9669998
mzlnk <dev.zielonka.marcin@gmail.com>
2021-07-28 00:50:23 +0200

    http support

--- src/ts/views/Auth/SignIn.tsx
+++ src/ts/views/Auth/SignIn.tsx
@@ -3,14 +3,14 @@ import Container from 'react-bootstrap/Container';
 import AuthView from './AuthView';
 import GoogleButton from '../../components/common/SubmitButton/IconButton/GoogleButton';
 import FacebookButton from '../../components/common/SubmitButton/IconButton/FacebookButton';
-import googleUrl from '../../auth/AuthCredentials';
+import {googleUrl, facebookUrl} from '../../auth/AuthCredentials';
 
 const SignIn = () => {
   return (
     <Container className="my-0">
       <AuthView title="Sign In">
         <GoogleButton redirectTo={googleUrl} text="Sign in with Google" className="my-2" />
-        <FacebookButton redirectTo="/signup" text="Sign in with Facebook" className="mt-4" />
+        <FacebookButton redirectTo={facebookUrl} text="Sign in with Facebook" className="mt-4" />
       </AuthView>
     </Container>
   );
