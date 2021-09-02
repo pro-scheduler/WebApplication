229b0431b627fb760b1f1c3d30f9065859d08a63
AlexMaz99 <ola_m10@onet.pl>
2021-09-02 22:39:14 +0200

    collapse meeting notifications

--- src/ts/components/MeetingDetails/MeetingNotifications/MeetingNotifications.tsx
+++ src/ts/components/MeetingDetails/MeetingNotifications/MeetingNotifications.tsx
@@ -7,6 +7,8 @@ import SurveyTimeReminder from './SurveyTimeReminder';
 import MeetingReminder from './MeetingReminder';
 import { TimeRangeDTO } from '../../../model/TimeRangeDTO';
 import CustomReminder from './CustomReminder';
+import { useState } from 'react';
+import { Collapse } from 'react-collapse';
 
 export type MeetingNotificationsProps = {
   meetingId: number;
@@ -25,48 +27,60 @@ const MeetingNotifications = ({
   markTimeRangeDeadline,
   meetingFinalDate,
 }: MeetingNotificationsProps) => {
+  const [opened, setOpened] = useState<boolean>(true);
+
   return (
     <>
       <Row className="justify-content mt-5 ml-5 pl-5">
-        <LineWithHeader header={'Notifications'} />
+        <LineWithHeader header={'Notifications'} collapseAction={setOpened} />
       </Row>
       <Row className="justify-content-center ml-5 pl-5">
         <Col sm={12}>
-          <CustomReminder meetingId={meetingId} meetingName={meetingName} />
+          <Collapse isOpened={opened}>
+            <CustomReminder meetingId={meetingId} meetingName={meetingName} />
+          </Collapse>
         </Col>
       </Row>
       <Row className="justify-content-center ml-5 pl-5">
         {markTimeRangeDeadline && (
           <Col sm={12}>
-            <MeetingTimeVotingReminder
-              meetingId={meetingId}
-              meetingName={meetingName}
-              deadline={new Date(markTimeRangeDeadline)}
-            />
+            <Collapse isOpened={opened}>
+              <MeetingTimeVotingReminder
+                meetingId={meetingId}
+                meetingName={meetingName}
+                deadline={new Date(markTimeRangeDeadline)}
+              />
+            </Collapse>
           </Col>
         )}
         {meetingFinalDate && (
           <Col sm={12}>
-            <MeetingReminder
-              meetingId={meetingId}
-              meetingName={meetingName}
-              finalDate={new Date(meetingFinalDate.timeStart)}
-            />
+            <Collapse isOpened={opened}>
+              <MeetingReminder
+                meetingId={meetingId}
+                meetingName={meetingName}
+                finalDate={new Date(meetingFinalDate.timeStart)}
+              />
+            </Collapse>
           </Col>
         )}
       </Row>
       {surveyId && (
         <Row className="justify-content-center ml-5 pl-5">
           <Col sm={12}>
-            <SurveyReminder meetingId={meetingId} surveyId={surveyId} meetingName={meetingName} />
+            <Collapse isOpened={opened}>
+              <SurveyReminder meetingId={meetingId} surveyId={surveyId} meetingName={meetingName} />
+            </Collapse>
           </Col>
           {surveyEndDate && (
             <Col sm={12}>
-              <SurveyTimeReminder
-                meetingId={meetingId}
-                surveyId={surveyId}
-                surveyEndDate={new Date(surveyEndDate)}
-              />
+              <Collapse isOpened={opened}>
+                <SurveyTimeReminder
+                  meetingId={meetingId}
+                  surveyId={surveyId}
+                  surveyEndDate={new Date(surveyEndDate)}
+                />
+              </Collapse>
             </Col>
           )}
         </Row>
