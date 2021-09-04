fbcb7e28fbc32d055339e46533d3f41d06e6cdff
AlexMaz99 <ola_m10@onet.pl>
2021-09-04 13:50:24 +0200

    editing user details

--- src/ts/components/UserProfile/UserDetails/UserDetails.tsx
+++ src/ts/components/UserProfile/UserDetails/UserDetails.tsx
@@ -1,14 +1,43 @@
 import { UserSummary } from '../../../model/user/ProUser';
 import Card from '../../common/Card/Card';
 import styles from './UserDetails.module.css';
+import { useEffect, useState } from 'react';
+import SingleValueInput from '../../common/forms/Input/SingleValueInput';
+import ActionButton from '../../common/SubmitButton/ActionButton/ActionButton';
 
 const UserDetails = ({ user }: { user: UserSummary }) => {
+  const [editMode, setEditMode] = useState<boolean>(false);
+  const [userName, setUserName] = useState<string>(user.username);
+
+  useEffect(() => {
+    setUserName(user.username);
+  }, [user.username]);
+
+  const editUserDetails = () => {
+    // TODO connect with API
+    setEditMode(false);
+  };
+
   return (
-    <Card title={'User details'}>
+    <Card title={'User details'} onEdit={() => setEditMode(!editMode)}>
       <div className={styles.userHeader}>Username</div>
-      <div>{user.username}</div>
-      <div className={styles.userHeader}>Email</div>
-      <div>{user.email}</div>
+      {editMode ? (
+        <>
+          <SingleValueInput valueHandler={setUserName} value={userName} />
+          <ActionButton
+            onclick={editUserDetails}
+            text={'Edit'}
+            disabled={userName === user.username || userName === ''}
+            className={styles.editButton}
+          />
+        </>
+      ) : (
+        <>
+          <div>{user.username}</div>
+          <div className={styles.userHeader}>Email</div>
+          <div>{user.email}</div>
+        </>
+      )}
     </Card>
   );
 };
