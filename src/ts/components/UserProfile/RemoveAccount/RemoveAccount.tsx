a7437d61cad8cd38c99171eaf9530087ee0d1e6e
AlexMaz99 <ola_m10@onet.pl>
2021-09-04 14:40:00 +0200

    added modal to removing account

--- src/ts/components/UserProfile/RemoveAccount/RemoveAccount.tsx
+++ src/ts/components/UserProfile/RemoveAccount/RemoveAccount.tsx
@@ -4,12 +4,18 @@ import SingleValueInput from '../../common/forms/Input/SingleValueInput';
 import { useState } from 'react';
 import ActionButton from '../../common/SubmitButton/ActionButton/ActionButton';
 import styles from './RemoveAccount.module.css';
+import YesNoPopup from '../../common/Popup/YesNoPopup';
+import { useHistory } from 'react-router-dom';
 
 const RemoveAccount = ({ user }: { user: UserSummary }) => {
   const [email, setEmail] = useState<string>('');
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const history = useHistory();
 
   const removeAccount = () => {
     // TODO connect with API
+    setShowModal(false);
+    history.push('/');
   };
 
   return (
@@ -24,12 +30,18 @@ const RemoveAccount = ({ user }: { user: UserSummary }) => {
           />
         </div>
         <ActionButton
-          onclick={removeAccount}
+          onclick={() => setShowModal(true)}
           text={'Remove account'}
           className={styles.removeAccountButton}
           disabled={user.email !== email}
         />
       </div>
+      <YesNoPopup
+        show={showModal}
+        title={'Are you sure you want to remove your account?'}
+        onAccept={removeAccount}
+        onDecline={() => setShowModal(false)}
+      />
     </Card>
   );
 };
