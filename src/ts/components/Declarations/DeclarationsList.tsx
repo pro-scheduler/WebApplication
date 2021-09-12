8b4a0363e8a27f956f08e43a2fc50bca983e9808
AlexMaz99 <ola_m10@onet.pl>
2021-09-12 14:34:28 +0200

    pencil icon in declarations list view

--- src/ts/components/Declarations/DeclarationsList.tsx
+++ src/ts/components/Declarations/DeclarationsList.tsx
@@ -1,12 +1,13 @@
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './DeclarationsList.module.css';
 import Card from '../common/Card/Card';
 import SearchBox from '../common/forms/Input/SearchBox';
 import { Table } from 'react-bootstrap';
 import { DeclarationDetails } from '../../model/declaration/Declaration';
 import { useHistory } from 'react-router';
+import PencilIcon from '../common/Icons/PencilIcon';
 
 export type DeclarationsListProps = {
   declarations: DeclarationDetails[];
@@ -45,27 +46,38 @@ const DeclarationsList = ({ declarations }: DeclarationsListProps) => {
   });
 
   return (
-    <Row className="justify-content-center mt-4 ml-sm-5">
-      <Col>
-        <Card title="Your declarations">
-          {declarations.length > 0 && (
-            <div className={styles.declarationsTable}>
-              <SearchBox value={searchTerm} onChange={handleChange} />
-              <Table responsive="sm" className="mt-4">
-                <thead>
-                  <tr>
-                    <th>Title</th>
-                    <th>Description</th>
-                    <th>Created by</th>
-                  </tr>
-                </thead>
-                <tbody>{declarationsRows}</tbody>
-              </Table>
-            </div>
-          )}
-        </Card>
-      </Col>
-    </Row>
+    <>
+      <Row className="justify-content-center mt-4 mr-5" style={{ marginLeft: '6%' }}>
+        <Col lg={12} className="text-center mt-5">
+          <PencilIcon className={styles.pencilIcon} />
+        </Col>
+      </Row>
+      <Row className="justify-content-center mt-4 ml-sm-5">
+        <Col>
+          <Card title="Your declarations">
+            {declarations.length > 0 ? (
+              <div className={styles.declarationsTable}>
+                <SearchBox value={searchTerm} onChange={handleChange} />
+                <Table responsive="sm" className="mt-4">
+                  <thead>
+                    <tr>
+                      <th>Title</th>
+                      <th>Description</th>
+                      <th>Created by</th>
+                    </tr>
+                  </thead>
+                  <tbody>{declarationsRows}</tbody>
+                </Table>
+              </div>
+            ) : (
+              <div className="text-center mt-3">
+                <div>You don't have any declarations</div>
+              </div>
+            )}
+          </Card>
+        </Col>
+      </Row>
+    </>
   );
 };
 
