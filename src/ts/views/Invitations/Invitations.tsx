3312ba7a8703ed16221ebb23409d325f91f6e3c4
AlexMaz99 <ola_m10@onet.pl>
2021-10-26 10:59:17 +0200

    SCH-355; removed unnecessary icons in list views

--- src/ts/views/Invitations/Invitations.tsx
+++ src/ts/views/Invitations/Invitations.tsx
@@ -5,8 +5,6 @@ import { fetchUserPendingInvitations } from '../../API/invitation/invitationServ
 import { UserSummary } from '../../model/user/ProUser';
 import { InvitationDetails } from '../../model/invitation/Invitation';
 import Col from 'react-bootstrap/Col';
-import EnvelopIcon from '../../components/common/Icons/EnvelopIcon';
-import styles from './Invitations.module.css';
 import Row from 'react-bootstrap/Row';
 import { ApiCall } from '../../API/genericApiCalls';
 import LoadingSpinner from '../../components/common/Spinner/LoadingSpinner';
@@ -26,11 +24,6 @@ const Invitations = ({ user }: { user: UserSummary }) => {
 
   return (
     <Container fluid>
-      <Row className="justify-content-center mt-4 mb-5">
-        <Col lg={12} className="text-center mt-5">
-          <EnvelopIcon className={styles.invitationListIcon} />
-        </Col>
-      </Row>
       {invitationsResponse.isSuccess ? (
         <Row className="justify-content-center mt-4">
           <Col>
