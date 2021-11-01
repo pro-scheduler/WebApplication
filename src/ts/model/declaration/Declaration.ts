5924a7158a9068ccaba9b02a9ba9c3f60bfba9ad
AlexMaz99 <ola_m10@onet.pl>
2021-11-01 19:08:17 +0100

    added meeting name to declarations view

--- src/ts/model/declaration/Declaration.ts
+++ src/ts/model/declaration/Declaration.ts
@@ -3,6 +3,7 @@ import { UserSummary } from '../user/ProUser';
 export type DeclarationDetails = {
   id: number;
   meetingId: number;
+  meetingName: string;
   title: string;
   description: string;
   createdBy: UserSummary;
