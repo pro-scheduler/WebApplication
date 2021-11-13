663928d5dc9d11f18ec7acaa05522fd97cc09e46
mzlnk <dev.zielonka.marcin@gmail.com>
2021-11-13 14:35:56 +0100

    SCH-350; simplified invitation link login flow

--- src/ts/views/Auth/SignIn.tsx
+++ src/ts/views/Auth/SignIn.tsx
@@ -3,14 +3,29 @@ import AuthView from './AuthView';
 import GoogleButton from '../../components/common/SubmitButton/IconButton/GoogleButton';
 import FacebookButton from '../../components/common/SubmitButton/IconButton/FacebookButton';
 import { googleUrl, facebookUrl } from '../../auth/AuthCredentials';
+import { useLocation } from 'react-router';
 
 const SignIn = () => {
+  const location = useLocation();
+
+  const constructStateParam = () => {
+    const queryParams = new URLSearchParams(location.search);
+    if (queryParams.get('invite-link')) {
+      return `t=il,v=${queryParams.get('invite-link')}`;
+    }
+    return `t=d,v=null`;
+  };
+
   return (
     <Container className="my-0">
       <AuthView title="Sign In">
-        <GoogleButton redirectTo={googleUrl} text="Sign in with Google" containerClassName="my-2" />
+        <GoogleButton
+          redirectTo={googleUrl.replace(':state', constructStateParam())}
+          text="Sign in with Google"
+          containerClassName="my-2"
+        />
         <FacebookButton
-          redirectTo={facebookUrl}
+          redirectTo={facebookUrl.replace(':state', constructStateParam())}
           text="Sign in with Facebook"
           containerClassName="mt-4"
         />
